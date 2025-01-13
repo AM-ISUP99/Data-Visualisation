abcde3659c52183c18764eee1516e424395f2344
AM-ISUP99 <63.masse.andrea@gmail.com>
2025-01-13 16:51:41 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -13,7 +13,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.feature_selection import SelectFromModel
 from keplergl import KeplerGl
 import geopandas as gpd
-import request
+import requests
 
 # Configuration de la page
 st.set_page_config(
