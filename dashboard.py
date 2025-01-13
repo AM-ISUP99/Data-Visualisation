2ee9ff3d3ed4842b393555b71fc868b38b91aef9
AM-ISUP99 <63.masse.andrea@gmail.com>
2025-01-13 16:28:31 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -51,11 +51,8 @@ def create_kepler_map():
     @st.cache_data
     def load_geo_data():
         # Charger les données géographiques des pays
-        world = gpd.read_file("ne_110m_admin_0_countries/ne_110m_admin_0_countries.shp")
-
-        if "ADMIN" in world.columns:
-                world.rename(columns={"ADMIN": "name"}, inplace=True)
-
+        world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+        
         # Liste complète des colonnes numériques
         numeric_columns = [
             'Life_expectancy', 'Adult_mortality', 'Infant_deaths', 
